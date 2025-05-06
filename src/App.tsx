8dd446263938dc8c595914affee67d8ed58f28b7
Elisha Winner <elisha.offically@gmail.com>
2025-05-06 15:19:54 +0100

    2nd commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello from your React !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
